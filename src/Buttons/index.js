650f98f1bf0b033f182ee5d4ccabf216cf1a89c4
Weronika Brożek <brozekweronika@gmail.com>
2022-03-19 17:11:41 +0100

    added react.fragment in Buttons

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,18 +1,20 @@
 import "./style.css";
 
 const Buttons = (props) => (
-    props.tasks.length > 0 && (
-        <span className="section__buttons">
-            <button className="section__button">{props.hideDoneTasks ? "Pokaż" : "Ukryj"} ukończone
-            </button>
-            <button
-                className="section__button"
-                disabled={props.tasks.every(({ done }) => done)}
-            >
-                Ukończ wszystkie
-            </button>
-        </span>
-    )
+    <div className="section__buttons">
+        {props.tasks.length > 0 && (
+            <>
+                <button className="section__button">{props.hideDoneTasks ? "Pokaż" : "Ukryj"} ukończone
+                </button>
+                <button
+                    className="section__button"
+                    disabled={props.tasks.every(({ done }) => done)}
+                >
+                    Ukończ wszystkie
+                </button>
+            </ >
+        )}
+    </div>
 );
 
 export default Buttons;
