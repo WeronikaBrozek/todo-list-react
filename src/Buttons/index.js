2ce9b1fd55519b5293d4f821061d586572d2786f
Weronika Brożek <brozekweronika@gmail.com>
2022-03-19 12:27:59 +0100

    fixed the typo in function name

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,18 +1,18 @@
 import "./style.css";
 
-const Buttons = (props) => {
-    return props.tasks.lenght > 0 && (
+const Buttons = (props) => (
+    props.tasks.length > 0 && (
         <span className="section__buttons">
             <button className="section__button">{props.hideDoneTasks ? "Pokaż" : "Ukryj"} ukończone
             </button>
-            <button 
-            className="section__button"
-            disabled={props.tasks.every(({ done }) => done)}
+            <button
+                className="section__button"
+                disabled={props.tasks.every(({ done }) => done)}
             >
-            Ukończ wszystkie
+                Ukończ wszystkie
             </button>
         </span>
     )
-};
+);
 
 export default Buttons;
