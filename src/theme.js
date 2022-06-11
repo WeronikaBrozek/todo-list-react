8b9ad1052de43c211a365fea0868e50070fade1b
Weronika Brożek <brozekweronika@gmail.com>
2022-06-11 11:00:59 +0200

    created theme file

---
+++ src/theme.js
@@ -0,0 +1,16 @@
+export const theme = {
+    color: {
+        white: "#fff",
+        teal: "#008080",
+        nobel: "#b3b3b3", //179
+        java: "#23c2c2", //active button 35, form active background color
+        gossamer: "#059191", //form background color
+        mineShaft: "#2c2c2c", //form active border.
+        mercury: "#e3e3e3", //section box shadow, border bottom,
+        crimson: "#dc143c", //tasks-button-backgroundcolor,
+        amaranth: "#eb3b5e", //tasks hover,
+        tundora: "#414141", //tasks active border,
+        green: "#06BC06", //tasks background color toggle done,
+        japaneseLaurel: "#039903", //tasks hover background,
+    }
+};
