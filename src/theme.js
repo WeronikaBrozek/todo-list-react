a04e0e6431939e5e2c41d82888f95e20af288b15
Weronika Brożek <brozekweronika@gmail.com>
2022-06-11 11:05:52 +0200

    removed comments

--- src/theme.js
+++ src/theme.js
@@ -2,15 +2,15 @@ export const theme = {
     color: {
         white: "#fff",
         teal: "#008080",
-        nobel: "#b3b3b3", //179
-        java: "#23c2c2", //active button 35, form active background color
-        gossamer: "#059191", //form background color
-        mineShaft: "#2c2c2c", //form active border.
-        mercury: "#e3e3e3", //section box shadow, border bottom,
-        crimson: "#dc143c", //tasks-button-backgroundcolor,
-        amaranth: "#eb3b5e", //tasks hover,
-        tundora: "#414141", //tasks active border,
-        green: "#06BC06", //tasks background color toggle done,
-        japaneseLaurel: "#039903", //tasks hover background,
+        nobel: "#b3b3b3", 
+        java: "#23c2c2", 
+        gossamer: "#059191", 
+        mineShaft: "#2c2c2c", 
+        mercury: "#e3e3e3", 
+        crimson: "#dc143c", 
+        amaranth: "#eb3b5e", 
+        tundora: "#414141", 
+        green: "#06BC06", 
+        japaneseLaurel: "#039903", 
     }
 };
